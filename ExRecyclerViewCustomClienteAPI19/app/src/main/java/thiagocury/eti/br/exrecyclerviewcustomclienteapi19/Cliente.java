07d7d73318149dd4fb39e0d9d0192e24967ac8d3
Thiago  Espindola Cury <thiagocury@MacBook-Pro-de-Thiago-2.local>
2017-04-24 19:44:33 -0300

    Exemplo RecyclerView + ClienteAdapter extends RecyclerView customizado - API19

---
+++ ExRecyclerViewCustomClienteAPI19/app/src/main/java/thiagocury/eti/br/exrecyclerviewcustomclienteapi19/Cliente.java
@@ -0,0 +1,54 @@
+package thiagocury.eti.br.exrecyclerviewcustomclienteapi19;
+
+/**
+ * Created by thiagocury on 16/04/17.
+ */
+
+public class Cliente {
+
+    private String nome;
+    private String sexo;
+    private double renda;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String sexo, double renda) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.renda = renda;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", renda=" + renda +
+                '}';
+    }
+}
