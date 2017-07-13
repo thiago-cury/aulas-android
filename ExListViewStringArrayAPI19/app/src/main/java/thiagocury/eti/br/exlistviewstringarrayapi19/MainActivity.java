cb8c92d5465fccc3efd5945b796786b35b520cb8
Thiago Cury <thiagocury@gmail.com>
2017-07-13 14:30:21 -0300

    Exemplo ListView preenchida através de StringArray - API19

---
+++ ExListViewStringArrayAPI19/app/src/main/java/thiagocury/eti/br/exlistviewstringarrayapi19/MainActivity.java
@@ -0,0 +1,16 @@
+package thiagocury.eti.br.exlistviewstringarrayapi19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Os valores serão preenchidos na lista a partir do atributo entries.
+           Essa opção foi inserido no XML activity_main.xml */
+    }
+}
